0b68d784db9dbde1e47b6dc40ccfa7de4bf41187
foool <navylq@163.com>
2013-04-15 08:51:06 +0800

    add two .h

---
+++ property.h
@@ -0,0 +1,3 @@
+bool next_permutation(vector<int> &v);
+bool DR_property(GMatrix mat, int n, int k, int beta);
+bool CL_S_property(GMatrix mat, int n, int k, int beta, vector<int> vi);
---
+++ utils.h
@@ -0,0 +1,1 @@
+void sort_v_i(vector<int> &v);
